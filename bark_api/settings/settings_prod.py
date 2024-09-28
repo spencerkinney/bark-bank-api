877ec8ccce6b107971fdffb53dd4b08fdbab83fc
Spencer Kinney <spencer_kinney@yahoo.com>
2024-09-27 22:33:53 -0700

    use Heroku proxy SSL redirect

--- bark_api/settings/settings_prod.py
+++ bark_api/settings/settings_prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['bark.com', 'barkbankapi-e88bfd94ccc1.herokuapp.com']
 
-# Security settings
+# Security settings for production
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
@@ -12,7 +12,16 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
 
-# Would typically define a production database here
+# Ensure Django knows it's behind a proxy and should trust the 'X-Forwarded-Proto' header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Static files settings (ensure Whitenoise is used for serving static files)
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Example production database setup
+# Uncomment and configure the following when using a production database like PostgreSQL
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -23,3 +32,5 @@ X_FRAME_OPTIONS = 'DENY'
 #         'PORT': os.environ.get('DB_PORT'),
 #     }
 # }
+
+# Additional settings for REST Framework, CORS, etc., inherit from settings_common
