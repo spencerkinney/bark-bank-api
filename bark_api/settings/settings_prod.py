c46c03b6becf980eea92b27c6c253fda20e47173
Spencer Kinney <spencer_kinney@yahoo.com>
2024-09-27 23:09:31 -0700

    Added Django Postgres adapter

--- bark_api/settings/settings_prod.py
+++ bark_api/settings/settings_prod.py
@@ -1,4 +1,6 @@
 from .settings_common import *
+import dj_database_url
+import os
 
 DEBUG = False
 
@@ -16,17 +18,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# Example production database setup
-# Uncomment and configure the following when using a production database like PostgreSQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
+# Production database setup - Use DATABASE_URL from Heroku
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 # Additional settings for REST Framework, CORS, etc., inherit from settings_common
